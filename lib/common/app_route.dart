8492d8d74ef80b5b649988ca8bb1fb832b2bd426
Deni Bouk <52736149+DeniBouk@users.noreply.github.com>
2021-10-17 19:44:01 +0700

    Update app_route.dart

--- lib/common/app_route.dart
+++ lib/common/app_route.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import '../pages/search_page.dart';
 import '../pages/login_page.dart';
 import '../ui/display_post_screen.dart';
 
 class AppRoute {
   static const loginRoute = '/login';
   static const homeRoute = '/home';
+  static const searchRoute = '/search';
   static final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
     loginRoute: (BuildContext context){
       return const LoginPage();
     },
     homeRoute: (BuildContext context){
       return const DisplayPost();
-    }
+    },
+    searchRoute: (BuildContext context){
+      return const SearchPage();
+  }
   };
 }
