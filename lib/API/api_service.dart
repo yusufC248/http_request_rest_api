b7532994d488a618382f8c78ac804e19cfbfcce7
Deni Bouk <52736149+DeniBouk@users.noreply.github.com>
2021-10-17 20:32:07 +0700

    Update api_service.dart

--- lib/API/api_service.dart
+++ lib/API/api_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../model/post_model.dart';
+import '../model/search.dart';
 import '../model/user.dart';
 
 class ApiService {
